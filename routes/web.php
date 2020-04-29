f23d66665dee347d99befaf5501c15d499318ccb
Angel Rodriguez <angelrodriguez@Angels-MacBook-Pro-2.local>
2020-04-28 23:27:29 -0400

    fixed route closure error

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/siteData', 'SiteController@getData')->name('site.getData');
 
 Route::get('/departmentData', 'DepartmentController@getData')->name('department.getData');
